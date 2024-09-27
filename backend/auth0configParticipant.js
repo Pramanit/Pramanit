121c577bd084a09686b9a3427b01a61e279b03cb
Gitesh <gitesh.sarvaiya28@gmail.com>
2024-09-27 17:19:06 +0530

    migration of frontend

--- backend/auth0configParticipant.js
+++ backend/auth0configParticipant.js
@@ -2,7 +2,7 @@ module.exports = {
     authRequired: false,
     auth0Logout: true,
     secret: 'a long, randomly-generated string stored in env',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000/participant',
     clientID: 'HfaoJ8O4DGatCTroguTbCZIue8aM7e72',
     issuerBaseURL: 'https://dev-wkux0vns83hhvzha.us.auth0.com'
   };
