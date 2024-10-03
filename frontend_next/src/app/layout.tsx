1f3c653e5d073b33521d49c7ac9a63876e8257ee
Akash Dewangan <123660848+akashdewgn@users.noreply.github.com>
2024-10-03 18:08:53 +0530

    link preview

--- frontend_next/src/app/layout.tsx
+++ frontend_next/src/app/layout.tsx
@@ -8,6 +8,25 @@ const dmSans = DM_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Pramanit",
   description: "Certificate Verification System",
+  icons: {
+    icon: '/favicon.ico', // Path to your favicon
+  },
+  openGraph: {
+    title: 'Pramanit',
+    description: 'Certificate Verification System',
+    url: 'https://pramanit.co',
+    siteName: 'Pramanit',
+    images: [
+      {
+        url: 'https://pramanit.co/hero.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Our homepage',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
